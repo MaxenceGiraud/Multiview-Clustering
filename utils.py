26d2af2cb811bcd24077e9b4368f3ba1878ea094
MaxenceGiraud <maxence9giraud@gmail.com>
2021-01-31 14:23:28 +0100

    Implement Spectral clustering with normalized laplacian and precomputed similarity matrix

---
+++ utils.py
@@ -0,0 +1,24 @@
+from sklearn.cluster import KMeans
+import numpy as np
+
+class SpectralClustering:
+    ''' Minimal implementation of Spectral clustering with normalized laplacian and precomputed similarity matrix '''
+    def __init__(self,k):
+        self.k = k
+    
+    def fit_predict(self,X):
+        D = np.diag(np.sum(X,axis=1))
+        L =  D - X
+        Dinv_sqrt = np.sqrt(np.linalg.pinv(D))
+        L = np.eye(X.shape[0]) - Dinv_sqrt @ X @ Dinv_sqrt
+
+        # EigenDecomposition
+        eig_val,eig_vec = np.linalg.eig(L)
+
+        # Take eigenvector corresponding to k smallest eigenvalues
+        Xnew = eig_vec[:,np.argsort(eig_val)[:2]].real
+
+        k = KMeans(3)
+        y_hat = k.fit_predict(Xnew)
+
+        return y_hat
